8c9e373d6320a5077506bafa355074a01f519af0
Juan Pablo Garcia Plaza Perez <jnpblgpz@gmail.com>
2024-08-30 15:18:29 +0200

    Improve script

--- censor.js
+++ censor.js
@@ -1,32 +1,62 @@
-const fs = require('fs')
+const { readFileSync, writeFileSync } = require('fs')
 
-const ITEM_TYPES = {login: 1, note: 2, card: 3, identity: 4}
+// Bitwarden Version: 2024.8.0 / Server Version: 2024.8.0
 
-// CENSORING SCRIPT
-
-const vault = require(`./${process.argv[2]}`)
-
-for (let itemIndex in vault["items"]) {
-    let item = vault["items"][itemIndex]
-
-    if (
-        item["type"] === ITEM_TYPES.note ||
-        item["type"] === ITEM_TYPES.card ||
-        item["type"] === ITEM_TYPES.identity
-    ) {
-        delete vault["items"][itemIndex]
-        continue;
-    }
-
-    delete item["passwordHistory"]
-    delete item["notes"]
-    delete item["fields"]
-
-    let login = item.type === ITEM_TYPES.login ? item["login"] : undefined
-
-    delete login["fido2Credentials"]
-    delete login["password"]
-    delete login["totp"]
+const ITEM_TYPES_ENUM = {
+    LOGIN: 1,
+    NOTE: 2,
+    CARD: 3,
+    IDENTITY: 4
 }
 
-fs.writeFileSync(`./bitwarden_export_censored_${new Date().toISOString().substring(0, 10)}.json`, JSON.stringify(vault))
+if (process.argv.slice(2).length !== 1) {
+    console.log("Usage: node censor.js <bitwarden_export.json>")
+    process.exit(1)
+}
+
+// 1. Parse vault JSON vault export
+
+let vault = undefined
+try {
+    const vaultContent = readFileSync(process.argv[2], 'utf8')
+    try {
+        vault = JSON.parse(vaultContent)
+    } catch (SyntaxError) {
+        console.log(`File ${process.argv[2]} is not a valid JSON file`)
+        process.exit(1)
+    }
+} catch (FileNotFoundException) {
+    console.log(`File ${process.argv[2]} not found`)
+    process.exit(1)
+}
+
+if (vault === undefined || !Object.keys(vault).length || !vault.hasOwnProperty('items')) {
+    console.log('Invalid or empty vault export')
+    process.exit(1)
+}
+
+const vaultItems = vault['items']
+
+// 2. Censor sensitive information
+
+vaultItems.forEach((item, index) => {
+    if (item['type'] !== ITEM_TYPES_ENUM.LOGIN) {
+        delete vaultItems[index]
+        return
+    }
+
+    delete item['passwordHistory']
+    delete item['notes']
+    delete item['fields']
+
+    const loginInfo = item['login']
+
+    delete loginInfo["fido2Credentials"]
+    delete loginInfo["password"]
+    delete loginInfo["totp"]
+})
+
+// 3. Write censored vault export to file
+
+const currentTimestamp = new Date().toISOString().replace(/:/g, '-').replace(/\./g, '-')
+writeFileSync(`./bitwarden_export_censored_${currentTimestamp}.json`, JSON.stringify(vault))
